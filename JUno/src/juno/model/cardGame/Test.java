cdadae131f18db17bceaf2694eff10c6574a3bd1
dadna <damianomailswork@gmail.com>
2023-03-30 11:32:24 +0200

    test for uno game

--- JUno/src/juno/model/cardGame/Test.java
+++ JUno/src/juno/model/cardGame/Test.java
@@ -11,19 +11,22 @@ public class Test {
 	public static void main(String[] args) {
 		
 		//array di giocatori va costrutito in un metodo di avvio della partita
-		Player playerUser = new Player("val7e");
+		// Player playerUser = new Player("val7e");
+		System.out.println("cioao");
 		Player playerBot0 = new Player("jim");
 		Player playerBot1 = new Player("pam");	
 		Player playerBot2 = new Player("dwight");
 			
 		List<Player> players = new ArrayList<Player>();
-		players.add(playerUser);
+		// players.add(playerUser);
 		players.add(playerBot0);
 		players.add(playerBot1);
 		players.add(playerBot2);
 		
-		UnoGame game1 = new UnoGame(players);
 		
+		UnoGame game = new UnoGame(players);
+		game.prepareGame();
+		game.startGame(null);
 		
 		
 	} 
